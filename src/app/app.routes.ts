e7d3eec6f0a8a945ccc95bd832074f2f01b10cc9
channoufi-saber <saber.channoufi@esprit.tn>
2026-06-20 17:35:29 +0100

    Forgot, Reset Password & Routes Protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,18 +9,25 @@ import { UpdatePassword } from './components/update-password/update-password';
 import { BookConsultation } from './components/book-consultation/book-consultation';
 import { MyAppointments } from './components/my-appointments/my-appointments';
 import { ConsultationHistory } from './components/consultation-history/consultation-history';
+import { ForgotPassword } from './components/forgot-password/forgot-password';
+import { ResetPassword } from './components/reset-password/reset-password';
+import { authGuard, patientOnlyGuard } from './services/guard';
 
 export const routes: Routes = [
   { path: 'register', component: Reg },
   { path: 'register-doctor', component: DoctorReg },
   { path: 'login', component: Login },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'reset-password', component: ResetPassword },
   { path: 'home', component: Home },
   { path: '', component: Home },
 
-  { path: 'profile', component: Profile },
-  { path: 'update-profile', component: UpdateProfile },
-  { path: 'update-password', component: UpdatePassword },
-  { path: 'book-appointment', component: BookConsultation },
-  { path: 'my-appointments', component: MyAppointments },
-  { path: 'consultation-history', component: ConsultationHistory },
+  { path: 'profile', component: Profile, canActivate: [patientOnlyGuard] },
+  { path: 'update-profile', component: UpdateProfile, canActivate: [patientOnlyGuard] },
+  { path: 'update-password', component: UpdatePassword, canActivate: [authGuard] },
+  { path: 'book-appointment', component: BookConsultation, canActivate: [patientOnlyGuard] },
+  { path: 'my-appointments', component: MyAppointments, canActivate: [patientOnlyGuard] },
+  { path: 'consultation-history', component: ConsultationHistory, canActivate: [patientOnlyGuard] },
+
+  { path: '**', component: Home },
 ];
---
+++ src/app/components/forgot-password/forgot-password.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ApiService } from '../../services/api-service';
+
+@Component({
+  selector: 'app-forgot-password',
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './forgot-password.html',
+  styleUrl: './forgot-password.css',
+})
+export class ForgotPassword {
+  formData = {
+    email: '',
+  };
+
+  error = '';
+  success = '';
+
+  constructor(
+    private apiService: ApiService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  handleSubmit(event: Event): void {
+    event.preventDefault();
+    this.error = '';
+    this.success = '';
+
+    this.apiService.forgetPassword(this.formData).subscribe({
+      next: (response: any) => {
+        if (response.statusCode === 200) {
+          this.success = 'Password reset instructions have been sent to your email';
+          this.formData = { email: '' };
+        } else {
+          this.error = response.message || 'Failed to sent reset instructions';
+        }
+        this.apiService.logout();
+        this.cdr.detectChanges();
+      },
+      error: (error: any) => {
+        this.error = error.error?.message || 'An error occurred while processing your request';
+        this.cdr.detectChanges();
+      },
+    });
+  }
+}
